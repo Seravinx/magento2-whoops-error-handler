1b306501369604e6bc5e1c3d35cbca743853f8df
Seravin <seravin@localhost.localdomain>
2018-01-28 23:57:56 +0300

    some changes

---
+++ Model/Config/Source/Editors.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Seravinx\Whoops\Model\Config\Source;
+
+class Editors implements \Magento\Framework\Option\ArrayInterface
+{
+    public function toOptionArray()
+    {
+        return [
+            ['label' => 'sublime', 'value' => 'sublime'],
+            ['label' => 'textmate', 'value' => 'textmate'],
+            ['label' => 'emacs', 'value' => 'emacs'],
+            ['label' => 'macvim', 'value' => 'macvim'],
+            ['label' => 'phpstorm', 'value' => 'phpstorm'],
+            ['label' => 'idea', 'value' => 'idea'],
+            ['label' => 'vscode', 'value' => 'vscode'],
+        ];
+    }
+}
--- Plugin/ErrorHandler.php
+++ Plugin/ErrorHandler.php
@@ -9,15 +9,32 @@ class ErrorHandler
 {
 	protected $appState;
 
-	public function __construct(\Magento\Framework\App\State $appState) {
+	protected $request;
+
+	protected $scopeConfig;
+
+	public function __construct(
+		\Magento\Framework\App\State $appState,
+		\Magento\Framework\App\RequestInterface $request,
+		\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+		) {
 		$this->appState = $appState;
+		$this->request = $request;
+		$this->scopeConfig = $scopeConfig;
 	}
 
 	public function beforelaunch() {
 		if ($this->appState->getMode() == \Magento\Framework\App\State::MODE_DEVELOPER) {
 			$run = new \Whoops\Run;
-			$handler = new \Whoops\Handler\PrettyPageHandler;
-			$handler->setEditor('sublime');
+			
+			if ($this->request->isXmlHttpRequest()) {
+				$handler = new \Whoops\Handler\JsonResponseHandler;
+			} else {
+				$handler = new \Whoops\Handler\PrettyPageHandler;
+				$handler->setEditor(
+					$this->scopeConfig->getValue('dev/debug/default_editor')
+				);
+			}
 			$run->pushHandler($handler);
 			$run->register();
 		}
